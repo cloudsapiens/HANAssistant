f2da650113048bc7cebb5cf34c8a688edf70797f
cloudSAPiens <72522600+cloudsapiens@users.noreply.github.com>
2020-10-30 16:00:34 +0100

    Delete lex-audio.js

--- HANAssistant-Lex-Polly-Integration-UseCase/lib/lex-audio.js
+++
@@ -1,8 +0,0 @@
-/**
- * @module LexAudio
- * @description The global namespace for Amazon Lex Audio
- */
-global.LexAudio = global.LexAudio || {};
-global.LexAudio.audioControl = require('./control.js').audioControl;
-global.LexAudio.conversation = require('./conversation.js').conversation;
-module.exports = global.LexAudio;
